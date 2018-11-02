f8ff84b550a9bfa620775e2a3db94f2d5f1d82ec
pim <10088685+nelftart@users.noreply.github.com>
2018-11-02 15:31:14 +0000

    Delete pixel_thing.m

--- recolour/pixel_thing.m
+++
@@ -1,5 +0,0 @@
-imtool('data/eucalyptus/out_01_12_2968.766846_1512.262085_.png');
-imtool('data/eucalyptus/out_01_13_2968.469238_1505.287231_.png');
-
-imtool('Results/LF/reco_out_01_12_2968.766846_1512.262085_.png');
-imtool('Results/LF/reco_out_01_13_2968.469238_1505.287231_.png');
